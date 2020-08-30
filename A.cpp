dded58b277eaeb75d6569537c0af19b222dcfe22
Shin <stariver0812@gmail.com>
2020-08-30 11:04:03 +0900

    fix variable name

--- A.cpp
+++ A.cpp
@@ -6,15 +6,15 @@ using namespace std;
 
 #define int long long
 
-double dist(int x1, int y1, int x2, int y2) {
-    return sqrt(pow(x1- x2, 2.0) + pow(y1 - y2, 2.0));
+double dist(double x1, double y1, double x2, double y2) {
+    return sqrt(pow(x1- x2, 2) + pow(y1 - y2, 2));
 }
 
 signed main() {
-    int x1, y1, r, x2, y2, x3, y3;
+    double x1, y1, r, x2, y2, x3, y3;
     cin >> x1 >> y1 >> r >> x2 >> y2 >> x3 >> y3;
 
-    if(x2 <= x1 - r && x1 + r <= x3 && y2 <= y1 - r && y1 + r<= x3) {
+    if(x2 <= x1 - r && x1 + r <= x3 && y2 <= y1 - r && y1 + r<= y3) {
         cout << "NO" << endl;
     } else {
         cout << "YES" << endl;
